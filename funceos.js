c7ad7f32e136e8f7a1f6ab7294a40197a635519f
FelipeAlvesG <55899912+FelipeAlvesG@users.noreply.github.com>
2019-12-09 12:42:06 -0300

    Update funceos.js

--- funceos.js
+++ funceos.js
@@ -3,11 +3,11 @@ function sala(){
     if(estado_sala === "0"){
         document.getElementById("estado_sala").innerHTML="1";
         document.getElementById("sala").innerHTML="<div class='sala_ligado'></div>";
-		document.getElementById("botao-sala").innerHTML="<a href='/?desligar_sala' class='botao'>Desliga</a>";
+		document.getElementById("botao-sala").innerHTML="<a href='/?desligar_sala/?desligar_sala' class='botao'>Desliga</a>";
     } else {
         document.getElementById("estado_sala").innerHTML="0";
         document.getElementById("sala").innerHTML="<div class='sala_desligado'></div>";
-        document.getElementById("botao-sala").innerHTML="<a href='/?ligar_sala' class='botao'>Liga</a>";
+        document.getElementById("botao-sala").innerHTML="<a href='/?ligar_sala/?ligar_sala' class='botao'>Liga</a>";
     }
 }
 
